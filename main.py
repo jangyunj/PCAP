2f55740e896326d487b4a3114cd135dbfee58584
Yun Jang <jang.yunj7@gmail.com>
2023-05-07 10:42:29 -0400

    unpacking

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # tuple - ordered collection - objects that belong together
-#       - cannot be changed - immutable
+#       - cannot be changed - IMMUTABLE
 #       - needs to be more than one OR have comma at the end if it only has one item
 
-mytuple = tuple(["Max", 28, "Boston"])  # converts from list to tuple
-print(mytuple)
+tuple1 = ("Max", 28, "Boston")
 
-for i in mytuple:
-    print(i)
+# Unpacking
+name, age, city = tuple1
+print(name)
+print(age)
+print(city)
 
-if "Tim" in mytuple:
-    print("yes")
-else:
-    print("no")
+tuple2 = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+i1, *i2, i3 = tuple2
+print(i1)
+print(i2)  # all of the elements in BETWEEN output in a list
+print(i3)
