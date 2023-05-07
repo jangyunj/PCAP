0c94e411aa18d4201f8220cc5f520e0363ab3306
Yun Jang <jang.yunj7@gmail.com>
2023-05-07 10:26:52 -0400

    tuple2

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 # tuple - ordered collection - objects that belong together
-#       - cannot be changed
+#       - cannot be changed - immutable
 #       - needs to be more than one OR have comma at the end if it only has one item
 
-mytuple = "Max",
+mytuple = tuple(["Max", 28, "Boston"])  # converts from list to tuple
 print(mytuple)
 
-print(type(mytuple))
+for i in mytuple:
+    print(i)
 
-
-mytuple1 = tuple(["Max", 28, "Boston"])
-print(mytuple1)
-
-item = mytuple1[2]
-print(item)
+if "Tim" in mytuple:
+    print("yes")
+else:
+    print("no")
