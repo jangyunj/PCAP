1f04d04bab0b651a8c86adecdef894a9eaccae1a
Yun Jang <jang.yunj7@gmail.com>
2023-05-07 00:26:38 -0400

    sort,sorted

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #lists
 
-mylist = ["banana", "cherry", "apple"]
+mylist = [5, 6, 1, 8, 14,2]
+
+
+item = mylist.sort()
 print(mylist)
 
-item = mylist[-2]
-print(item)
-
-for i in mylist:
-    print(i)
+new_list = sorted(mylist)
+print(new_list)
