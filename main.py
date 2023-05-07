2742992748cde1f352ab8dd58df944f03a761623
Yun Jang <jang.yunj7@gmail.com>
2023-05-07 10:51:33 -0400

    getsizeof,timeit

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 # tuple - ordered collection - objects that belong together
 #       - cannot be changed - IMMUTABLE
 #       - needs to be more than one OR have comma at the end if it only has one item
+#       - can be efficient when working with large data
 
-tuple1 = ("Max", 28, "Boston")
+import sys
 
-# Unpacking
-name, age, city = tuple1
-print(name)
-print(age)
-print(city)
+my_list = [1, 2, 3]
+my_tuple = (1, 2, 3)
+print(sys.getsizeof(my_list), "bytes")
+print(sys.getsizeof(my_tuple), "bytes")
 
-tuple2 = (1, 2, 3, 4, 5, 6, 7, 8, 9)
-i1, *i2, i3 = tuple2
-print(i1)
-print(i2)  # all of the elements in BETWEEN output in a list
-print(i3)
+import timeit
+print(timeit.timeit(stmt="[1, 2, 3]", number=1000000)) #TIME OF one million times we want to create this list
+print(timeit.timeit(stmt="(1, 2, 3)", number=1000000)) #TIME OF one million times we want to create this tuple
