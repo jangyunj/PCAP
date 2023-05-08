1f3ee217f8c9bcf2d99e250d1d16edcfad9d38ae
Yun Jang <jang.yunj7@gmail.com>
2023-05-08 00:38:01 -0400

    difference

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-odds = {1, 3, 5, 7, 9}
-evens = {0, 2, 4, 6, 8}
-primes = {2, 3, 5, 7}
 
-u1 = odds.union(evens) #combination - no duplicates of same numbers
-print("union: ", u1)
+setA = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+setB = {1, 2, 3, 10, 11, 12}
 
-u2 = odds.union(primes) #combination - no duplicates of same numbers
-print("union: ", u2)
+diff1 = setA.difference(setB) #Takes from setA - setB
+print(diff1)
 
-i = evens.intersection(primes) #commonality
-print(f"intersection: {i}")
+diff2 = setB.difference(setA) #Takes from setB - setA
+print(diff2)
+
+diff3 = setA.symmetric_difference(setB) #Takes everything that's not overlapping
+print(diff3)
+
+diff4 = setB.symmetric_difference(setA) #Same as diff3
+print(diff4)
+
+setA.update(setB) #Without duplicates, adds elements found in another set
+print(setA)
+
+setA.intersection_update(setB) #Elements found in both sets
+print(setA)
+
+setA.difference_update(setB) #Updates the set by removing elements found in another set
+print(setA)
+
+setA.symmetric_difference_update(setB) #Updates the set by output of elements in A & B after removing duplicates
+print(setA)
