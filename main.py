af484e5a9c40bb2aa04f9dc0da09ddb4b6f6d861
Yun Jang <jang.yunj7@gmail.com>
2023-05-09 21:53:51 -0400

    string

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
+#Backslash indicates that string should continue and not be on a new line
 
-setA = {1, 2, 3, 4, 5, 6}
-
-setB = setA    #both points to the same location
-setB.add(7)
-
-print(setB)
-print(setA)    #also affected
-
-#----OR----
-setB = setA.copy()
-
-#----OR----
-setB = set(setA)
+my_string = """Hello \
+World"""
+print(my_string)
