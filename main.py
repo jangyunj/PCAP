864763a78c879dcbf37201602be541fb0d4256b4
Yun Jang <jang.yunj7@gmail.com>
2023-05-09 21:47:30 -0400

    copy of set

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 
 setA = {1, 2, 3, 4, 5, 6}
-setB = {1, 2, 3}
-setC = {7, 8}
-print(setA.issubset(setB)) #Are all elements in first set in second set?
 
-print(setB.issubset(setA))
+setB = setA    #both points to the same location
+setB.add(7)
 
-print(setB.issuperset(setA)) #Opposite of subset
+print(setB)
+print(setA)    #also affected
 
-print(setA.isdisjoint(setB)) #Are there any common elements?
+#----OR----
+setB = setA.copy()
 
-print(setA.isdisjoint(setC))
+#----OR----
+setB = set(setA)
