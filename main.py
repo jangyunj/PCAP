e4093e31626c7bbb7994bd30abf51377c4860847
Yun Jang <jang.yunj7@gmail.com>
2023-05-09 21:43:07 -0400

    sub,superset

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 
-setA = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-setB = {1, 2, 3, 10, 11, 12}
+setA = {1, 2, 3, 4, 5, 6}
+setB = {1, 2, 3}
+setC = {7, 8}
+print(setA.issubset(setB)) #Are all elements in first set in second set?
 
-diff1 = setA.difference(setB) #Takes from setA - setB
-print(diff1)
+print(setB.issubset(setA))
 
-diff2 = setB.difference(setA) #Takes from setB - setA
-print(diff2)
+print(setB.issuperset(setA)) #Opposite of subset
 
-diff3 = setA.symmetric_difference(setB) #Takes everything that's not overlapping
-print(diff3)
+print(setA.isdisjoint(setB)) #Are there any common elements?
 
-diff4 = setB.symmetric_difference(setA) #Same as diff3
-print(diff4)
-
-setA.update(setB) #Without duplicates, adds elements found in another set
-print(setA)
-
-setA.intersection_update(setB) #Elements found in both sets
-print(setA)
-
-setA.difference_update(setB) #Updates the set by removing elements found in another set
-print(setA)
-
-setA.symmetric_difference_update(setB) #Updates the set by output of elements in A & B after removing duplicates
-print(setA)
+print(setA.isdisjoint(setC))
