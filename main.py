b9754cb2efd0b41f035cb4fc9405726f0d274cbf
Yun Jang <jang.yunj7@gmail.com>
2023-05-09 22:13:57 -0400

    string methods

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 greeting = "Hello"
-name = "Tom"
-sentence = greeting + " " + name
-print(sentence)
 
-sentence1 = f"{greeting} {name}"
-print(sentence1) 
+for i in greeting:
+    print(i)
+
+if "ell" in greeting:
+    print("yes")
+else:
+    print("no")
+
+my_string = "      Hello World     "
+my_string = my_string.strip()  #You have to assign it to the original one (make copy), because string is IMMUTABLE
+print(my_string)
+
+print(my_string.upper())
+print(my_string.lower())
