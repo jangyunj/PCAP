13d3d159a101c4735c447e23dff7100d1301d100
Yun Jang <jang.yunj7@gmail.com>
2023-05-10 22:56:21 -0400

    namedtuple

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from collections import Counter
+from collections import namedtuple
 
-a = "aaabbbccccc"
-my_counter = Counter(a)
-print(my_counter)
-
-print(my_counter.most_common(1)) #returns list with tuples inside
-print(my_counter.most_common(1)[0][0]) #access the first list, first item
-
-print(list(my_counter.elements())) #iterable over all elements - so print as list 
+Point = namedtuple('Point', 'x,y')
+pt = Point(1, 4)
+print(pt.x, pt.y)
