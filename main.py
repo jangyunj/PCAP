d416530a7a3e3c9ecd2ba1a3363167b5ae7ff70e
Yun Jang <jang.yunj7@gmail.com>
2023-05-10 23:23:57 -0400

    deque

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from collections import defaultdict
+#Double ended que - add/remove elements from both ends
+from collections import deque
 
-#Returns specific type of value we specify
-d = defaultdict(int)
-d = defaultdict(float)
-d = defaultdict(list)
+d = deque()
 
-d['a'] = 1
-d['b'] = 2
-print(d['c'])
+d.append(1)
+d.append(2)
+d.appendleft(3)
+print(d)
+
+# d.pop()
+# d.popleft()
+# d.clear()
+# d.extend([4, 5, 6])
+d.extendleft([4, 5, 6]) #adds backwards
+print(d)
+
+d.rotate(1) #rotates (moves) 1 place to the RIGHT
+d.rotate(-1) #rotates 1 place to the LEFT
+print(d)
