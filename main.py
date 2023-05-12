bd0c05a30c2799b7d488a759082f843b5e13d951
Yun Jang <jang.yunj7@gmail.com>
2023-05-11 23:56:16 -0400

    itertools

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-#Double ended que - add/remove elements from both ends
-from collections import deque
-
-d = deque()
-
-d.append(1)
-d.append(2)
-d.appendleft(3)
-print(d)
-
-# d.pop()
-# d.popleft()
-# d.clear()
-# d.extend([4, 5, 6])
-d.extendleft([4, 5, 6]) #adds backwards
-print(d)
-
-d.rotate(1) #rotates (moves) 1 place to the RIGHT
-d.rotate(-1) #rotates 1 place to the LEFT
-print(d)
+from itertools import product
+a = [1, 4]
+b = [3, 4]
+prod = product(a, b)
+print(list(prod))
