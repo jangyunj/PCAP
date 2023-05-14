efacb063d3334c5da2973532f03526488700b2fc
Yun Jang <jang.yunj7@gmail.com>
2023-05-13 23:46:13 -0400

    permutations

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from itertools import product
-a = [1, 4]
-b = [3]
-prod = product(a, b, repeat=2)
-print(list(prod))
+from itertools import permutations
+a = [1, 2, 3]
+perm = permutations(a, 2)
+print(list(perm))
