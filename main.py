0e3d2494a5ab8f6898e31eb0a658e9c0f0398d51
Yun Jang <jang.yunj7@gmail.com>
2023-05-14 23:43:09 -0400

    end parameter

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-s = '0123456789'
-obj = range(0, len(s))  #(start, stop, (step)) 
-print(obj)
-l = []
-for i in obj:
-    l.append(int(s[i]))
-print(l)                #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print("python", end=" ")
+print("rules", end="!")
+print("hello")             #By default, Python's End Parameter is '\n'
+print("world")
 
-add = 0
-for i in obj:
-    add = add + l[i]    #Sums all numbers in l (above)
-print(add)
+''' RESULT: 
 
-#----------------------
-s = '0123456789'
-obj = range(0, len(s), 1)  #Error! The end argument cannot be zero - instead, you should put 1 or get rid of it 
-print(obj)
-l = []
-for i in obj:
-    l.append(int(s[i]))
-print(l)                
+python@rules!hello
+world
 
-add = 0
-for i in obj:
-    add = add + l[i]    
-print(add)
+'''
