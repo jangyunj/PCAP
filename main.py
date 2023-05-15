70d3a97e445be0bf2a284974df7e546874ff2bad
Yun Jang <jang.yunj7@gmail.com>
2023-05-14 23:35:40 -0400

    range2

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ add = 0
 for i in obj:
     add = add + l[i]    #Sums all numbers in l (above)
 print(add)
+
+#----------------------
+s = '0123456789'
+obj = range(0, len(s), 1)  #Error! The end argument cannot be zero - instead, you should put 1 or get rid of it 
+print(obj)
+l = []
+for i in obj:
+    l.append(int(s[i]))
+print(l)                
+
+add = 0
+for i in obj:
+    add = add + l[i]    
+print(add)
